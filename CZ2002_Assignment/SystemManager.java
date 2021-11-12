e161eb62252128aca6a5ec1b1248a34b3008128e
Bryan Chatsirichai <89973472+bryanChatsirichai@users.noreply.github.com>
2021-11-12 12:54:23 +0800

    Update SystemManager.java

--- CZ2002_Assignment/SystemManager.java
+++ CZ2002_Assignment/SystemManager.java
@@ -528,7 +528,7 @@ public class SystemManager
 	{
 		for(int i = 0; i < tableList.size(); i++) 
 		{						
-			ViewReservation(i+1);
+			ViewReservation(i);
 			System.out.println("-------------------------");
 		}
 	}
