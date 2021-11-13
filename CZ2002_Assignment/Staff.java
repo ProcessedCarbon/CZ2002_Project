7ac3880de5a6f021ce9ce5047e076f383d25811c
joash-lee <91481123+joash-lee@users.noreply.github.com>
2021-11-13 13:52:53 +0800

    Update Staff.java

--- CZ2002_Assignment/Staff.java
+++ CZ2002_Assignment/Staff.java
@@ -23,27 +23,62 @@ public class Staff {
 		this.emp_id = emp_id;
 	}
 	
+	/**
+	* Returns the name of the Staff
+	*/
 	public String getName() {
 		return name;
 	}
+	
+	/**
+	* Returns the gender of the Staff
+	*/
 	public String getGender() {
 		return gender;
 	}
+	
+	/**
+	* Returns the job title of the Staff
+	*/
 	public String getJobTitle() {
 		return jobTitle;
 	}
+	
+	/**
+	* Returns the employee id of the Staff
+	*/
 	public int getEmp_id() {
 		return emp_id;
 	}
+	
+	/**
+	* Sets the name of the Staff to the input
+	* @para name name of Staff 
+	*/
 	public void setName(String name) {
 		this.name = name;
 	}
+	
+	/**
+	* Sets the gender of the Staff to the input
+	* @para name gender of Staff 
+	*/
 	public void setGender(String gender) {
 		this.gender = gender;
 	}
+	
+	/**
+	* Sets the job title of the Staff to the input
+	* @para jobTitle job title of Staff 
+	*/
 	public void setJobTitle(String jobTitle) {
 		this.jobTitle = jobTitle;
 	}
+	
+	/**
+	* Sets the employee id of the Staff to the input
+	* @para emp_id employee id of Staff 
+	*/
 	public void setEmp_id(int emp_id) {
 		this.emp_id = emp_id;
 	}
