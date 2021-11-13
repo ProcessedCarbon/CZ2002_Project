0139b169700060453839bcff8cff8bba15152aa5
tanjiahao1357 <93370680+tanjiahao1357@users.noreply.github.com>
2021-11-13 15:53:27 +0800

    Update Reservation.java

--- CZ2002_Assignment/Reservation.java
+++ CZ2002_Assignment/Reservation.java
@@ -14,6 +14,9 @@ public class Reservation {
 	*/
 	private String date;
 	
+	/**
+	* The time of reservation
+	*/
 	LocalTime time;
 	
 	/**
@@ -32,7 +35,12 @@ public class Reservation {
 	private int tableNo;
 	
 	/**
-	* Class constructor for Reservation 
+	*Constructor for Class
+	* @param date Date of Reservation
+	* @param time Time of Reservation
+	* @param pax Total number of customers
+	* @param customer Customer who made the Reservation
+	* @param tableNo Table Number of Reservation
 	*/
 	public Reservation(String date, LocalTime time, int pax, Customer customer, int tableNo)
 	{
@@ -43,46 +51,91 @@ public class Reservation {
 		this.tableNo = tableNo;
 	}
 	
+	/**
+	 * Gets the date of reservation
+	 * @return date of reservation
+	 */
 	public String getReservationDate()
 	{
 		return date;
 	}
+	
+	/**
+	 * Sets the date of reservation
+	 * @param string of date
+	 */
 	public void setReservationDate(String date)
 	{
 		this.date = date;
 	}
 	
+	/**
+	 * Gets the time of reservation
+	 * @return time of reservation
+	 */
 	public LocalTime getReservationTime()
 	{
 		return time;
 	}
+	
+	/**
+	 * Sets the time of reservation
+	 * @param string of time
+	 */
 	public void setReservationTime(LocalTime time)
 	{
 		this.time = time;
 	}
 	
+	/**
+	 * Gets the total number of people for the reservation
+	 * @return total number of people
+	 */
 	public int getReservationPax()
 	{
 		return pax;
 	}
+	
+	/**
+	 * Sets the total number of people for the reservation
+	 * @param int total number of people
+	 */
 	public void setReservationPax(int pax)
 	{
 		this.pax = pax;
 	}
 	
+	/**
+	 * Gets Customer who made reservation
+	 * @return Customer who made reservation
+	 */
 	public Customer getReservationCustomer()
 	{
 		return customer;
 	}
+	
+	/**
+	 * Sets customer who made reservation
+	 * @param customer who made reservation
+	 */
 	public void setReservationCustomer(Customer customer)
 	{
 		this.customer = customer;
 	}
 	
+	/**
+	 * Gets table number of reservation
+	 * @return table number of reservation
+	 */
 	public int getReservationTable()
 	{
 		return tableNo;
 	}
+	
+	/**
+	 * Sets the table number of this reservation
+	 * @param int of table number
+	 */
 	public void setReservationTable(int tableNo)
 	{
 		this.tableNo = tableNo;
