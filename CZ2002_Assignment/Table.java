1e6f5a2954497dd4df2f3bcf2be3046c2f02e55c
joash-lee <91481123+joash-lee@users.noreply.github.com>
2021-11-13 13:56:43 +0800

    Update Table.java

--- CZ2002_Assignment/Table.java
+++ CZ2002_Assignment/Table.java
@@ -19,21 +19,48 @@ public class Table {
 		this.isOccupied = isOccupied;
 		this.isReserved = isReserved;
 	}
+	
+	/**
+	* Returns the seat capacity of the Table
+	*/
 	public int getSeatCapacity() {
 		return SeatCapacity;
 	}
+	
+	/**
+	* Returns the occupation status of the Table
+	*/
 	public boolean isOccupied() {
 		return isOccupied;
 	}
+	
+	/**
+	* Returns the reservation status of the Table
+	*/
 	public boolean isReserved() {
 		return isReserved;
 	}
+	
+	/**
+	* Sets the seat capacity of the Table to the input
+	* @para seatCapacity seat capacity of the Table
+	*/
 	public void setSeatCapacity(int seatCapacity) {
 		this.SeatCapacity = seatCapacity;
 	}
+	
+	/**
+	* Sets the occupation status of the Table to the input
+	* @para isOccupied occupation status of the Table
+	*/
 	public void setOccupied(boolean isOccupied) {
 		this.isOccupied = isOccupied;
 	}
+	
+	/**
+	* Sets the reservation status of the Table to the input
+	* @para isReserved reservation status of the Table
+	*/
 	public void setReserved(boolean isReserved) {
 		this.isReserved = isReserved;
 	}
